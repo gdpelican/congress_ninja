855d2da85cca6e225a35df5f2de7a74bf9055b93
James Kiesel <james.kiesel@gmail.com>
2016-12-02 14:27:57 -0500

    Use correct db endpoint

--- config/prod.exs
+++ config/prod.exs
@@ -2,10 +2,13 @@ use Mix.Config
 
 config :congress_ninja, CongressNinja.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "congress.ninja", port: 80],
+  url: [host: "congress-ninja.herokuapp.com", port: 80],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Do not print debug messages in production
+config :logger, level: :info
 
 # Configure your database
 config :congress_ninja, CongressNinja.Repo,
